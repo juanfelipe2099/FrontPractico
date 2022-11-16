6d11f4cd1fcbb2d3ade344913c12895db20c3900
Juan Felipe Alvarez <juanfelipe2099@gmail.com>
2022-11-15 19:47:29 -0500

    Antes de lo peligroso

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 burguerMenu.addEventListener('click',toggleMobileMenu);
@@ -49,12 +50,12 @@ function toggleCarritoAside(){
 const productList = [];
 productList.push({
     name: 'Barrel Brewing',
-    price: 2.00,
+    price: 2.99,
     image: 'https://images.unsplash.com/photo-1559180786-c6f26acc1111?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="" class="product-img',
 });
 productList.push({
     name: 'Strong Bear',
-    price: 4.00,
+    price: 4.99,
     image: 'https://images.unsplash.com/photo-1633710734156-a33cd91d7ac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="" class="product-img',
 });
 /*
@@ -72,6 +73,42 @@ productList.push({
             </div>
 */
 
-for(product of productList){
-    document.createElement('')
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+     
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src',product.image);
+     
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+     
+        const productInfoDiv = document.createElement('div');
+     
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+     
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+     
+        const productInfoFigure = document.createElement('figure');
+        
+        const imgFigure = document.createElement('img');
+        imgFigure.setAttribute('src','./iconos/Platzi_YardSale_Icons/bt_add_to_cart.svg');
+         
+         productInfoFigure.appendChild(imgFigure);
+         productInfo.appendChild(productInfoDiv);
+         productInfo.appendChild(productInfoFigure);
+         productCard.appendChild(productImg);
+         productCard.appendChild(productInfo);
+     
+         cardsContainer.appendChild(productCard);
+     
+     
+     }
 }
+
+renderProducts (productList);
